8e7fdec88c4a3925c199df9750023f93fa5ba941
Justin Kimbrell <jkimbrell@actengag.com>
2023-08-15 23:18:52 -0600

    chore: updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+export default {
     content: [
-        "./index.html",
-        "./demo/**.vue"
+        './index.html',
+        './demo/**.vue'
     ],
     theme: {
         extend: {},
